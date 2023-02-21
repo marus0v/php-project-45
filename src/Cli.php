e884a7ab71852dfd93a330f82e7842fbcf4d4b61
Andrey <andrey.marus@gmail.com>
2023-02-21 04:01:40 +0300

    stage 2, undefined function

--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once(__DIR__ . 'vendor/wp-cli/php-cli-tools/lib/cli/cli.php');
+// require_once(__DIR__ . 'vendor/wp-cli/php-cli-tools/lib/cli/cli.php');
 
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function welcomeUser($name)
+function welcomeUser()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
