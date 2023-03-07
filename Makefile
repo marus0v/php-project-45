bd7486c7778291afb54db020998e9fef7231d64b
Andrey <andrey.marus@gmail.com>
2023-03-07 16:33:16 +0300

    1 project 7 stage Working

--- Makefile
+++ Makefile
@@ -10,8 +10,11 @@ brain-games: # запустить brain-games
 brain-even: # запустить brain-even (проверка на четность)
 	./bin/brain-even
 
-brain-calc: # запустить brain-even (проверка на четность)
+brain-calc: # запустить brain-calc (калькулятор)
 	./bin/brain-calc
+
+brain-gcd: # запустить brain-gcd (поиск НОД)
+	./bin/brain-gcd
 
 lint: # установить CodeSniffer
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
---
+++ src/GCD.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace BrainGames\GCD;
+
+use function cli\line;
+use function BrainGames\Calc\getAnswer;
+
+function getValues() : array
+{
+    $num1 = mt_rand(0, 100);
+    $num2 = mt_rand(0, 100);
+    $values[] = $num1;
+    $values[] = $num2;
+    return $values;
+}
+function countValues($values) : array
+{
+    // $sortedValues = rsort($values);
+    $numA = $values[0];
+    $numB = $values[1];
+    echo ("Question: {$numA} {$numB} \n");
+    $gcd = 1;
+    if ($numA == $numB) {
+        $gcd = $numA;
+        $values[] = $gcd;
+        return $values;
+    } else {
+        while (($numA != 0) && ($numB != 0)) {
+            if ($numA > $numB) {
+                $numA = $numA % $numB;
+            } else if ($numB > $numA) {
+                $numB = $numB % $numA;
+            }
+        }
+        $gcd = $numA + $numB;
+        $values[] = $gcd;
+    }
+    return $values;
+}
+function gcdGame($userName)
+{
+    echo ("Find the greatest common divisor of given numbers. \n");
+    $winsNumber = 0;
+    while ($winsNumber < 3) {
+        $values = getValues();
+        $result = (countValues($values)[2]);
+        $ans = getAnswer();
+        if ($ans === $result) {
+            $winsNumber++;
+            line('Correct!');
+        } else {
+            echo ("'{$ans}' is wrong answer ;(. Correct answer was '{$result}' \n");
+            return line("Let's try again, %s!", $userName);
+        }
+    }
+    return line("Congratulations, %s!", $userName);
+}
+
