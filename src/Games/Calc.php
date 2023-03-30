aacbeeebebe27a07b419ab72d9b98bc141fd23d3
Andrey <andrey.marus@gmail.com>
2023-03-30 05:25:20 +0300

    Calc array_rand used

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -8,7 +8,7 @@ use function cli\prompt;
 function getValues(): array
 {
     $operations = ["+", "-", "*"];
-    $operation = $operations[mt_rand(0, 2)];
+    $operation = $operations[array_rand($operations, 1)];
     $num1 = mt_rand(0, 100);
     $num2 = mt_rand(0, 100);
     $values = array();
@@ -25,52 +25,25 @@ function countValues(array $values): array
         case "+":
             $question = "{$values[1]} + {$values[2]}";
             $qna[] = $question;
-            // line("Question: %s", $question);
             $result = $values[1] + $values[2];
             $qna[] = $result;
             break;
         case "-":
             $question = "{$values[1]} - {$values[2]}";
             $qna[] = $question;
-            // line("Question: %s", $question);
             $result = $values[1] - $values[2];
             $qna[] = $result;
             break;
         case "*":
             $question = "{$values[1]} * {$values[2]}";
             $qna[] = $question;
-            // line("Question: %s", $question);
             $result = $values[1] * $values[2];
             $qna[] = $result;
             break;
         default:
             throw new \Exception('No operator selected!');
     }
-    // $values[] = $result;
     return $qna;
-}
-function getAnswer(): string
-{
-    $answer = prompt('Your answer: ');
-    return $answer;
-}
-function calculateGame(string $userName)
-{
-    $winsNumber = 0;
-    line("What is the result of the expression?");
-    while ($winsNumber < 3) {
-        $values = getValues();
-        $result = (countValues($values)[3]);
-        $ans = getAnswer();
-        if ($ans === $result) {
-            $winsNumber++;
-            line('Correct!');
-        } else {
-            echo ("'{$ans}' is wrong answer ;(. Correct answer was '{$result}' \n");
-            return line("Let's try again, %s!", $userName);
-        }
-    }
-    return line("Congratulations, %s!", $userName);
 }
 
 function runCalcGame(): array
