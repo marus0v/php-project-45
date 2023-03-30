041388a4ae8aa0994400b1b76ff3e772f9036445
Andrey <andrey.marus@gmail.com>
2023-03-30 06:58:49 +0300

    Progression const fixed

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -5,6 +5,9 @@ namespace BrainGames\Progression;
 use function cli\line;
 use function BrainGames\Engine\getAnswer;
 
+const TOTALWINSNUMBER = 3;
+const DESCRIPTION = "What number is missing in the progression?";
+
 function getValues(): array
 {
     $num1 = mt_rand(1, 100);
@@ -16,38 +19,6 @@ function getValues(): array
     }
     $str_values = implode(" ", $values);
     return $values;
-}
-function hideValue(array $values): array
-{
-    $qna = array();
-    $i = mt_rand(0, 9);
-    $hid_values = $values;
-    $hid_values[$i] = "..";
-    // $values[10] = $values[$i];
-    // $str_values = implode(" ", $hid_values);
-    $qna[] = implode(" ", $hid_values);
-    $qna[] = $values[$i];
-    // line("Question: %s!", $str_values);
-    return $qna;
-}
-
-function proGame(string $userName)
-{
-    echo ("What number is missing in the progression? \n");
-    $winsNumber = 0;
-    while ($winsNumber < 3) {
-        $values = getValues();
-        $result = (hideValue($values)[10]);
-        $ans = getAnswer();
-        if ($ans == $result) {
-            $winsNumber++;
-            line('Correct!');
-        } else {
-            echo ("'{$ans}' is wrong answer ;(. Correct answer was '{$result}' \n");
-            return line("Let's try again, %s!", $userName);
-        }
-    }
-    return line("Congratulations, %s!", $userName);
 }
 
 function getQandA(array $values): array
@@ -64,8 +35,8 @@ function runProGame()
 {
     $counter = 1;
     $questionsAndAnswers = array();
-    $questionsAndAnswers[0] = "What number is missing in the progression?";
-    while ($counter < 4) {
+    $questionsAndAnswers[0] = DESCRIPTION;
+    while ($counter < (TOTALWINSNUMBER + 1)) {
         $values = getValues();
         $questionsAndAnswers[$counter] = getQandA($values);
         $counter++;
